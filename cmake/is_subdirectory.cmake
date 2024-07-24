fce97a3f41d32fe1a3b371ecd05e8746d921676e
gomkyung2 <stripe2933@gmail.com>
2024-07-24 19:19:05 +0900

    Fix is_subdirectory function.

--- cmake/is_subdirectory.cmake
+++ cmake/is_subdirectory.cmake
@@ -1,5 +1,5 @@
 function(is_subdirectory parent_abs sub_abs result)
-    if (sub_abs MATCHES "${parent_abs}$")
+    if ("${sub_abs}" MATCHES "${parent_abs}$")
         set(${result} TRUE PARENT_SCOPE)
     else()
         set(${result} FALSE PARENT_SCOPE)
