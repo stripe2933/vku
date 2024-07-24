ad55f91b973223f0c0e8b465ee2533795f9f44ce
gomkyung2 <stripe2933@gmail.com>
2024-07-24 19:14:03 +0900

    Fix is_subdirectory function.

--- cmake/is_subdirectory.cmake
+++ cmake/is_subdirectory.cmake
@@ -1,32 +1,10 @@
 function(is_subdirectory parent_dir sub_dir result)
-    # Normalize paths
-    get_filename_component(parent_abs "${parent_dir}" ABSOLUTE)
-    get_filename_component(sub_abs "${sub_dir}" ABSOLUTE)
+    cmake_path(ABSOLUTE_PATH parent_dir NORMALIZE OUTPUT_VARIABLE parent_abs)
+    cmake_path(ABSOLUTE_PATH sub_dir NORMALIZE OUTPUT_VARIABLE sub_abs)
 
-    # Initial check for path equality
-    if (parent_abs STREQUAL sub_abs)
+    if (sub_abs MATCHES "${parent_abs}$")
+        set(${result} TRUE PARENT_SCOPE)
+    else()
         set(${result} FALSE PARENT_SCOPE)
-        return()
     endif()
-
-    # Iterate up the directory tree from sub_dir
-    set(current_dir "${sub_abs}")
-    while (NOT current_dir STREQUAL parent_abs)
-        get_filename_component(current_dir "${current_dir}" DIRECTORY)
-
-        # If we reach the root and haven't matched, it's not a subdirectory
-        if (current_dir STREQUAL "/")
-            set(${result} FALSE PARENT_SCOPE)
-            return()
-        endif()
-
-        # If the current_dir is equal to the parent_dir, then sub_dir is a subdirectory
-        if (current_dir STREQUAL parent_abs)
-            set(${result} TRUE PARENT_SCOPE)
-            return()
-        endif()
-    endwhile()
-
-    # Default case if nothing else matches
-    set(${result} FALSE PARENT_SCOPE)
 endfunction()
